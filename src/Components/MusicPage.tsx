e341d4cf0d0865482cde3cfe130ebb58fb910eae
Matt Ruder <matt.j.ruder@gmail.com>
2022-08-26 14:07:34 -0600

    Add basic components setup

---
+++ src/Components/MusicPage.tsx
@@ -0,0 +1,9 @@
+function MusicPage() {
+  return (
+    <div>
+      <h1>This is the music page</h1>
+    </div>
+  )
+}
+
+export default MusicPage
---
+++ src/Components/SoftwarePage.tsx
@@ -0,0 +1,9 @@
+function SoftwarePage() {
+  return (
+    <div>
+      <h1>This is the software page</h1>
+    </div>
+  )
+}
+
+export default SoftwarePage
